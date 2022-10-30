3b06faafa27f4e02f5d8deb26b5ecd63372d7ec9
BradMyrick <brad.myrick@protonmail.com>
2022-10-30 16:54:15 -0500

    init next 13 mint app

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
+  plugins: [],
+}
