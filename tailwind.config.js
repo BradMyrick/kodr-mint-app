853f599317de3d12e75c5ddfa1b11c770b8902e2
BradMyrick <brad.myrick@protonmail.com>
2022-10-31 07:48:04 -0500

    start again

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
-  plugins: [],
-}
